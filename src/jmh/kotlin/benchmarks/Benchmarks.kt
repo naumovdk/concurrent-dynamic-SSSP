3281aa911b6b4781f859e505b217d8ffa8ec27c5
Dmitrii Naumov <dimnau@gmail.com>
2021-11-01 11:36:56 +0300

    Change graph set

--- src/jmh/kotlin/benchmarks/Benchmarks.kt
+++ src/jmh/kotlin/benchmarks/Benchmarks.kt
@@ -29,7 +29,7 @@ open class SmallBenchmark {
     @Param("0.5", "0.9", "0.99")
     open var readWriteRatio: Double = 0.0
 
-    @Param("USA", "NY", "WEST")
+    @Param("WEST", "NY")
     open var graphName: String = ""
 
     @Param("0", "1", "2", "3")
