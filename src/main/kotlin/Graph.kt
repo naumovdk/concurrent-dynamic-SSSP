bcdff60404bc1e0c7f7ff325c75b881bfe104d02
Dmitrii Naumov <dimnau@gmail.com>
2021-11-01 11:24:28 +0300

    Change graphs path

--- src/main/kotlin/Graph.kt
+++ src/main/kotlin/Graph.kt
@@ -15,7 +15,7 @@ data class InputGraph(val nodes: Int, val edges: List<InputEdge>, val minWeight:
 class Graph {
     companion object {
 //        private const val path = "C:/Users/dimna/IdeaProjects/concurrent-dynamic-SSSP/graphs/"
-        private const val path = "./graphs/"
+        private const val path = "/home/dmitry.naumov/concurrent-dynamic-SSSP/graphs/"
 
         val emptyGraph = InputGraph(INITIAL_SIZE, listOf(), 0, 0)
 
